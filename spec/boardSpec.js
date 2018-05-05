b5642692afbe40cd1bd97774b2f5d8981515f789
solene basaia <bsolene@hotmail.fr>
2018-05-05 17:06:08 +0200

    set winning rows and columns with tests

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,11 +1,24 @@
 describe('Board', function () {
     var board;
     beforeEach(function () {
-        board = new Board(3, 3);
+        board = new Board(3);
     });
 
     it('should create a new board', function () {
         board.createBoard();
         expect(board.table.length).toEqual(9)
+    })
+
+    it('should create the winning combination for the rows', function () {
+        board.createBoard();
+        board.winningCombinationRows();
+        expect(board.winningRows.length).toEqual(3)
+    })
+
+    it('should create the winning combination for the columns', function () {
+        board.createBoard();
+        board.winningCombinationRows();
+        board.winningCombinationColumns();
+        expect(board.winningColumns.length).toEqual(3)
     })
 })
--- src/board.js
+++ src/board.js
@@ -1,14 +1,37 @@
 class Board {
-    constructor(x, y) {
-        this.numSquares = x * y;
+    constructor(dimensionBoard) {
+        this.numSquares = dimensionBoard * dimensionBoard;
         this.table = [];
+        this.winningRows = [];
+        this.winningColumns = [];
+        this.dimensionBoard = dimensionBoard;
     }
     createBoard() {
         for (let i = 0; i < this.numSquares; i++) {
             var square = i;
             this.table.push(square);
-        }
-    }
+        };
+    };
+
+    winningCombinationRows() {
+        for (let i = 0; i < this.table.length; i += this.dimensionBoard) {
+            this.winningRows.push(this.table.slice(i, i + this.dimensionBoard))
+        };
+    };
+
+    winningCombinationColumns() {
+        var sortedSquares = [];
+
+        for (let i = 0; i < this.dimensionBoard; i ++) {
+            for (let j = 0; j < this.dimensionBoard; j++) {
+                sortedSquares.push(this.winningRows[j][i])
+            };
+        };
+
+        for (let z = 0; z < sortedSquares.length; z += this.dimensionBoard) {
+            this.winningColumns.push(sortedSquares.slice(z, z + this.dimensionBoard))
+        };
+    };
 };
 
 
